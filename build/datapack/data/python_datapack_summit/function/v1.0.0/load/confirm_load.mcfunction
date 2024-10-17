13211e64650055fdd6e59391b4b9ed89e028a00b
Stoupy51 <stoupy51@gmail.com>
2024-10-17 22:02:12 +0200

    🚧 Adding text displays...

--- build/datapack/data/python_datapack_summit/function/v1.0.0/load/confirm_load.mcfunction
+++ build/datapack/data/python_datapack_summit/function/v1.0.0/load/confirm_load.mcfunction
@@ -7,3 +7,7 @@
 tellraw @a[tag=convention.debug] {"translate":"python_datapack_summit.loaded_pythondatapacksummit_v1_0_0","color":"green"}
 scoreboard players set #python_datapack_summit.loaded load.status 1
 
+# Display the text "welcoming"
+kill @e[tag=python_datapack_summit.welcoming,tag=python_datapack_summit]
+summon text_display 119 75 -117 {"Tags":['python_datapack_summit.welcoming', 'python_datapack_summit', 'smithed.entity', 'smithed.strict', 'global.ignore', 'global.ignore.strict'],"text":'[{"translate":"python_datapack_summit.welcome_to_the_python_datapack_booth", "color": "gold"}, {"text":"\\u1000", "color": "white", "font": "python_datapack_summit:text_display"}]'}
+
--- user/links/text_displays.py
+++ user/links/text_displays.py
@@ -3,16 +3,47 @@
 from python_datapack.utils.io import *
 
 # Constants
-TEXT_DISPLAYS: dict = {
-	"welcoming": {},
+CONVENTION_TAGS: list[str] = ["smithed.entity","smithed.strict","global.ignore","global.ignore.strict"]
+CUSTOM_FONTS: dict[str,str] = {
+	"icon": r"\u1000",
 }
 
 # Main function to place the text displays
 def main(config: dict) -> None:
 	namespace: str = config["namespace"]
-	build_datapack: str = config['build_datapack']
+	assets_folder: str = config["assets_folder"]
 
-	# TODO: Add the text displays
+	# Make the fonts
+	write_to_file(f"{config['build_resource_pack']}/assets/{namespace}/font/text_display.json", super_json_dump({
+		"providers": [
+			{"type":"bitmap","file":"python_datapack_summit:text_display/icon.png","ascent":0,"height":40,"chars":[CUSTOM_FONTS["icon"]]}
+		]
+	}).replace("\\\\", "\\"))
+	super_copy(f"{assets_folder}/original_icon.png", f"{config['build_resource_pack']}/assets/{namespace}/textures/text_display/icon.png")
+
+	# Text displays
+	TEXT_DISPLAYS: dict = {
+		"welcoming": {
+			"position": [119, 75, -117],
+			"text": [
+				{"text":"Welcome to the Python Datapack Booth!\n","color":"gold"},
+				{"text":CUSTOM_FONTS["icon"],"color":"white","font":f"{namespace}:text_display"},
+			],
+		},
+	}
+
+	# Add the text displays
+	for entity_id, data in TEXT_DISPLAYS.items():
+		x, y, z = data["position"]
+		text = json.dumps(data["text"])
+
+		# Write in load file the kill and summon commands
+		summon_tags: list[str] = [f"{namespace}.{entity_id}", namespace] + CONVENTION_TAGS
+		write_to_load_file(config, f"""
+# Display the text "{entity_id}"
+kill @e[tag={namespace}.{entity_id},tag={namespace}]
+summon text_display {x} {y} {z} {{"Tags":{summon_tags},"text":'{text}'}}
+""")
 
 	pass
 
